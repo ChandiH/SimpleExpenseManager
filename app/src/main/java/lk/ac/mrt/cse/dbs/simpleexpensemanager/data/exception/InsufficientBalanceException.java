c918412f26c164b625a71fbb5d7ee795f2de9ac9
Somesh Chandimal <someshchandih@gmail.com>
2022-12-12 22:43:06 +0530

    add new constructor for pass account balance through exception

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/exception/InsufficientBalanceException.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/exception/InsufficientBalanceException.java
@@ -4,8 +4,14 @@ package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception;
  * This exception is thrown in case of an insufficient balance for transaction
  */
 public class InsufficientBalanceException extends Exception{
+    public double amount;
     public InsufficientBalanceException(String detailMessage) {
         super(detailMessage);
+    }
+
+    public InsufficientBalanceException(String detailMessage, double amount){
+        super(detailMessage);
+        this.amount = amount;
     }
 
     public InsufficientBalanceException(String detailMessage, Throwable throwable) {
